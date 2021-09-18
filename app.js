fbd0d3f45dd1f07cdb99d4976927d6ca892a7c5f
Sandipan Das <61842142+sandip2224@users.noreply.github.com>
2021-09-18 08:41:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,5 +46,5 @@ app.use((req, res, next) => {
 app.use('/', require("./routes/index"))
 app.use('/users', require("./routes/users"))
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
